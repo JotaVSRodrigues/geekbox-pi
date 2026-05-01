74b113018e9dd43a4d6d342d896198017075831d
JotaVSRodrigues <joao.rdg119@gmail.com>
2026-04-30 23:17:38 -0300

    new fixes in dash and routes js

---
+++ backend/src/routes/estatisticas.js
@@ -0,0 +1,6 @@
+var express = require("express");
+var router = express.Router();
+
+var dashboardController = require("../controllers/dashboardController");
+
+// router.get("")
---
+++ frontend/js/dashboard.js
@@ -0,0 +1,14 @@
+/* 
+IDs
+kpi-concluidos
+kpi-horas
+kpi-semanal
+kpi-conclusao
+
+grafico-linha
+grafico-donut
+grafico-metas
+grafico-heatmap (uma div)
+
+class .chart
+*/
