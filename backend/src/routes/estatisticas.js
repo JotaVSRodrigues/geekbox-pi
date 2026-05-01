b7122643a0b38d52e44d948050da0623a3e53fe9
JotaVSRodrigues <joao.rdg119@gmail.com>
2026-04-30 23:33:36 -0300

    estatisticas routes + models and controllers directories + chartjs archive renamed

--- backend/src/routes/estatisticas.js
+++ backend/src/routes/estatisticas.js
@@ -1,6 +1,20 @@
 var express = require("express");
 var router = express.Router();
 
-var dashboardController = require("../controllers/dashboardController");
+var estatisticaController = require("../controllers/estatisticaController");
 
-// router.get("")
+router.get("/grafico/consumo-anual", function (req, res) {
+    estatisticaController.buscarConsumoAnual(req, res);
+});
+
+router.get("/grafico/horas-por-midia", function (req, res) {
+    estatisticaController.buscarHorasPorMidia(req, res);
+});
+
+router.get("/grafico/metas-por-ano", function (req, res) {
+    estatisticaController.buscarMetasPorAno(req, res);
+});
+
+router.get("/grafico/frequencia-consumo", function (req, res) {
+    estatisticaController.buscarFrequenciaConsumo(req, res);
+});
