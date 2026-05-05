7259d3aa3ed5c842760b111c345ff61caa888657
JotaVSRodrigues <joao.rdg119@gmail.com>
2026-05-04 22:27:09 -0300

    saudacao replace by sessao - same functins

--- frontend/js/sessao.js
+++ frontend/js/sessao.js
@@ -1,20 +1,48 @@
 
-function validarSessao() {
-    var nome = sessionStorage.NOME_USUARIO;
-    var email = sessionStorage.EMAIL_USUARIO;
-    var telefone = sessionStorage.TELEFONE_USUARIO;
-
-    // mostrar nome do usuário na tela
-    // var b_usuario = document.getElementById("b_usuario");
-
-    if (email != null && nome != null) {
-        // b_usuario.innerHTML = nome; 
+document.addEventListener("DOMContentLoaded", function() {
+    
+    const horas = new Date();
+    const horaAtual = horas.getHours();
+    let mensagem = "";
+    
+    if (horaAtual < 12) {
+        mensagem = "Bom dia, ";
+    } else if (horaAtual < 18) {
+        mensagem = "Boa tarde, ";
     } else {
-        window.location.href = "../login.html";
+        mensagem = "Boa noite, ";
     }
-}
+    
+    document.getElementById("mensagem-hora").innerText = mensagem;
+    
+    var nomeUsuario = sessionStorage.getItem("NOME_USUARIO");
+    var elementoNome = document.getElementById("h1-span");
+    
+    if (nomeUsuario && elementoNome) {
+        var primeiroNome = nomeUsuario.split(" ")[0];
+        elementoNome.innerText = primeiroNome + "!";
+    } else if (elementoNome) {
+        elementoNome.innerText = "visitante!";
+    }
+});
 
-function limparSessao() {
-    sessionStorage.clear();
-    window.location.href = "../login.html";
-}
+// function validarSessao() {
+//     var nome = sessionStorage.NOME_USUARIO;
+//     var email = sessionStorage.EMAIL_USUARIO;
+//     var telefone = sessionStorage.TELEFONE_USUARIO;
+
+//     // mostrar nome do usuário na tela
+//     // var b_usuario = document.getElementById("b_usuario");
+
+//     if (email != null && nome != null) {
+//         // b_usuario.innerHTML = nome; 
+//     } else {
+//         window.location.href = "../login.html";
+//     }
+// }
+
+// function limparSessao() {
+//     sessionStorage.clear();
+//     window.location.href = "../login.html";
+// }
+
