232d02af9ba6b930c30822eb3e20e536f4b614a0
JotaVSRodrigues <joao.rdg119@gmail.com>
2026-05-04 23:27:31 -0300

    directories replaced in web-data-viz pattern

---
+++ public/js/login.js
@@ -0,0 +1,68 @@
+
+function loginUser() {
+    // let emailTemp = "joao@gmail.com"
+    // let senhaTemp = "12345"
+
+    let emailLogin = ipt_email_login.value
+    let senhaLogin = ipt_senha_login.value 
+
+
+    if (emailLogin == "" || senhaLogin == "") {
+        alert("Há algum campo vazio. Preencha para prosseguir")
+
+        // mensagem_erro.innerHTML = "(Mensagem de erro para todos os campos em branco)";
+        return false
+    }
+
+    // if (senhaLogin != senhaTemp || emailLogin != emailTemp) {
+    //     alert("Email ou senha não correspondem")
+
+    //     // mensagem_erro.innerHTML = "(Mensagem de erro para todos os campos em branco)";
+    //     return false;
+    // } 
+
+    fetch("/usuarios/autenticar", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            emailServer: emailLogin,
+            senhaServer: senhaLogin
+        })
+    }).then(function (resposta) {
+        console.log("ESTOU NO THEN DO loginUser()")
+
+        if (resposta.ok) {
+            console.log(resposta);
+
+            resposta.json().then(json => {
+                console.log("dados recebidos do backend", json);
+                console.log(JSON.stringify(json));
+
+                // limpa sessões anteriores
+                sessionStorage.clear();
+                
+                sessionStorage.ID_USUARIO = json.id;
+                sessionStorage.NOME_USUARIO = json.nome; 
+                sessionStorage.EMAIL_USUARIO = json.email;
+                sessionStorage.TELEFONE_USUARIO = json.telefone;
+
+                setTimeout(function() {
+                    window.location.href = "../index.html";
+                }, 1000); // apenas para exibir o loading
+            });
+        } else {
+            console.log("Houve um erro ao tentar realizar o login!");
+            // colocar mensagem amigavel para o usuario
+
+            resposta.text().then(texto => {
+                console.error(texto);
+            });
+        }
+    }).catch(function (erro) {
+        console.log(erro);
+    })
+
+    return false;
+}
---
+++ src/routes/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+const path = require("path");
+
+router.get("/", function (req, res) {
+    // res.json({ message: 'API working nice' });
+    // res.sendFile(path.join(__dirname, "../login.html"));
+    res.render("index");
+})
+
+module.exports = router
